a63d376b23c9f6babafc2abe051d108cee6345a8
paul.andrei.onac <onac.paul.andrei@gmail.com>
2023-03-05 18:23:45 +0200

    test

--- themes/schitul-mironesti/assets/js/index.js
+++ themes/schitul-mironesti/assets/js/index.js
@@ -13,16 +13,16 @@ import 'bootstrap';
 // Fancybox
 //---------------------------------------------
 
-import { Fancybox, Carousel, Panzoom } from "@fancyapps/ui";
-Fancybox.bind('[data-fancybox="gallery"]', {
-  caption: (fancybox, slide) => {
-    const caption = slide.caption || "";
+// import { Fancybox, Carousel, Panzoom } from "@fancyapps/ui";
+// Fancybox.bind('[data-fancybox="gallery"]', {
+//   caption: (fancybox, slide) => {
+//     const caption = slide.caption || "";
 
-    return `${slide.index + 1} / ${
-      fancybox.carousel?.slides.length
-    } <br /> ${caption}`;
-  },
-});
+//     return `${slide.index + 1} / ${
+//       fancybox.carousel?.slides.length
+//     } <br /> ${caption}`;
+//   },
+// });
 
 //---------------------------------------------
 // Custom
