95cea4446e1ddd7befc8a8602c846dcff07ffec3
andrei.onac <onac.paul.andrei@gmail.com>
2025-08-31 12:41:33 +0300

    refactor: Update assets folder scheme

---
+++ assets/scripts/vendor/fancybox.js
@@ -0,0 +1,9 @@
+import { Fancybox, Carousel, Panzoom } from "@fancyapps/ui";
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+  caption: function (fancybox, carousel, slide) {
+    return (
+      `${slide.index + 1} / ${carousel.slides.length} <br />` + slide.caption
+    );
+  },
+});
